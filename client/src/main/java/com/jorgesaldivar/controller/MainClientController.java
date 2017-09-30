a40a6239794541c62f4da3e65717fc9040bb5337
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2017-09-30 10:04:25 -0500

    Added client

---
+++ client/src/main/java/com/jorgesaldivar/controller/MainClientController.java
@@ -0,0 +1,35 @@
+package com.jorgesaldivar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@RestController
+public class MainClientController {
+
+	private EurekaClient eurekaClient;
+	private RestTemplateBuilder restTemplateBuilder;
+
+	@Autowired
+	public MainClientController(EurekaClient eurekaClient, RestTemplateBuilder restTemplateBuilder) {
+		this.eurekaClient = eurekaClient;
+		this.restTemplateBuilder = restTemplateBuilder;
+	}
+
+	@RequestMapping("/")
+	public String callService() {
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("service", false);
+		String baseUrl = instanceInfo.getHomePageUrl();
+		ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
+		return response.getBody();
+	}
+
+}
